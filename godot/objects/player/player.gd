374fcc21be4e8a39e13a9a01db5cb01619e5f4f7
John <jastadj@gmail.com>
2022-10-29 06:15:19 -0700

    initial commit

---
+++ godot/objects/player/player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var allow_input = true
+var move_speed = 5000
+
+func _physics_process(delta):
+	
+	if allow_input: handle_input(delta)
+	
+func handle_input(delta):
+	
+	var move_dir = Vector2()
+	
+	if Input.is_action_pressed("ui_up"): move_dir.y -= 1
+	if Input.is_action_pressed("ui_down"): move_dir.y += 1
+	if Input.is_action_pressed("ui_left"): move_dir.x -= 1
+	if Input.is_action_pressed("ui_right"): move_dir.x += 1
+	
+	
+	
+	move_and_slide(move_dir*move_speed*delta)
